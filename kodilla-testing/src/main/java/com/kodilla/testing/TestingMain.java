58756ed97e265adc3dfb9a5e328deec8a24fdce6
KarolDmowski <dmowski.ka@gmail.com>
2017-12-21 03:13:40 +0100

    Kodilla 6.2 Calc test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,32 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+
+
+        if (Calculator.addCalc(7,3) == 10) {
+            System.out.println("Add test OK");
+        } else {
+            System.out.println("Add test Error");
+        }
+
+        if (Calculator.substractCalc(-15,-35) == 20){
+            System.out.println("Substract test OK");
+        } else {
+            System.out.println("Substract test Error");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,15 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    public static int addCalc(int firstNumber, int secondNumber){
+        int result = 0;
+        result = firstNumber + secondNumber;
+        return result;
+    }
+
+    public static int substractCalc(int firstNumber, int secondNumber){
+        int result = 0;
+        result = firstNumber - secondNumber;
+        return result;
+    }
+}
