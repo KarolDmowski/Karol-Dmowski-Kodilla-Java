c83f86dac3f21593105417871403de8ca181f152
KarolDmowski <dmowski.ka@gmail.com>
2018-02-13 20:35:46 +0100

    kodilla-spring 10.3 v2

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/AlwaysTrueCondition.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/AlwaysTrueCondition.java
@@ -1,4 +1,4 @@
-package com.kodilla.spring.reader;
+package com.kodilla.spring.portfolio;
 
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -1,6 +1,5 @@
 package com.kodilla.spring.portfolio;
 
-import com.kodilla.spring.reader.AlwaysTrueCondition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
